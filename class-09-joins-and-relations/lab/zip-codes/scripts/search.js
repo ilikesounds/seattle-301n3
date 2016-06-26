e63115204ba7e4352a330fa8c548a0b428a67f70
Jeff Torres <jeffrey.n.torres@gmail.com>
2016-06-26 11:57:37 -0700

    changed .empty to siblings remove

--- class-09-joins-and-relations/lab/zip-codes/scripts/search.js
+++ class-09-joins-and-relations/lab/zip-codes/scripts/search.js
@@ -2,10 +2,10 @@
 
 
 var Filter = {};
-  // TODO: Write the code to populate your filters, and enable the search queries here in search.js
-  // TODO: You will also interact with the map.js file here
+// TODO: Write the code to populate your filters, and enable the search queries here in search.js
+// TODO: You will also interact with the map.js file here
 
-Filter.populateState = function () {
+Filter.populateState = function() {
   webDB.execute('SELECT DISTINCT state FROM zips ORDER BY state ASC;', function(states) {
     var stateArr = [];
     states.map(function(a) {
@@ -16,25 +16,19 @@ Filter.populateState = function () {
     });
   })
   Filter.setCity();
-  }
+}
 
-Filter.setCity = function () {
+Filter.setCity = function() {
   var result;
   $('#state-select').on('change', function() {
-    // console.log("firing");
-    $("#city-select").empty();
-    $("#city-select").append('<option value="city">Select a City</option>');
+    $("#initial-city").siblings().remove();
     result = "'" + $(this).val() + "'";
-    console.log(result);
     Filter.populateCity(result);
   })
 }
 
 Filter.populateCity = function(ele) {
-  console.log(ele);
     webDB.execute('SELECT DISTINCT city FROM zips WHERE state =' + ele + ' ORDER BY city ASC;', function(cities) {
-      console.log("Cities should follow:");
-      console.log(cities);
       var cityArr = [];
       cities.map(function(a) {
         cityArr.push(a.city);
@@ -44,4 +38,4 @@ Filter.populateCity = function(ele) {
       })
     })
   }
-// })(window)
+  // })(window)
