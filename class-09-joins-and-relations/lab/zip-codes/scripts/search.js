57b2559f1a684616583f49783ea98cb2d91f671a
Jeff Torres <jeffrey.n.torres@gmail.com>
2016-06-26 13:09:59 -0700

    search box complete

--- class-09-joins-and-relations/lab/zip-codes/scripts/search.js
+++ class-09-joins-and-relations/lab/zip-codes/scripts/search.js
@@ -5,6 +5,28 @@ var Filter = {};
 // TODO: Write the code to populate your filters, and enable the search queries here in search.js
 // TODO: You will also interact with the map.js file here
 
+Filter.searchBox = function() {
+  var result;
+  $('form').on('submit', function(e) {
+    e.preventDefault();
+    result = $('#zipSearch').val();
+    if (isNaN(parseInt(result)) === true || result.length != 5) {
+      alert('You must enter a valid 5 digit zip code!');
+      result = null;
+    } else {
+      webDB.execute('SELECT zip, latitude, longitude FROM zips WHERE zip=' + result + ';', function(zipCodes) {
+        console.log(zipCodes);
+        if (zipCodes.length === 0) {
+          alert('That zip code does not exist in our database, please try another zip code.');
+          zipCodes = null;
+        } else {
+
+        }
+      })
+    }
+  })
+}
+
 Filter.populateState = function() {
   webDB.execute('SELECT DISTINCT state FROM zips ORDER BY state ASC;', function(states) {
     var stateArr = [];
