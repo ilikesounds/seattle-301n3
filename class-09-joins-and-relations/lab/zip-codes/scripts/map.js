80031702a431c67fc42eee909be19921c7ec68ee
Jeff Torres <jeffrey.n.torres@gmail.com>
2016-06-26 15:36:24 -0700

    stuff

--- class-09-joins-and-relations/lab/zip-codes/scripts/map.js
+++ class-09-joins-and-relations/lab/zip-codes/scripts/map.js
@@ -1,10 +1,29 @@
 function initMap() {
   // Create a map object and specify the DOM element for display.
   var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 47.611435, lng: -122.330456},
+    center: {
+      lat: 47.611435,
+      lng: -122.330456
+    },
     scrollwheel: true,
     zoom: 8
   });
 
   // TODO: Follow the Google Maps API docs to create markers on the map based on the search options on the home page.
+}
+
+function dropMarkers(lat, lng) {
+  var marker = {};
+  var myLatlng = new google.maps.LatLng(lat, lng);
+  var mapOptions = {
+    zoom: 4,
+    center: myLatlng
+  }
+  var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+  var marker['marker' + i] = new google.maps.Marker({
+    position: myLatlng,
+    map: map,
+    title: cityName,
+  });
+  marker.setMap(map);
 }
--- class-09-joins-and-relations/lab/zip-codes/scripts/search.js
+++ class-09-joins-and-relations/lab/zip-codes/scripts/search.js
@@ -1,6 +1,6 @@
 // (function(module) {
 
-
+var MarkerObj = {};
 var Filter = {};
 // TODO: Write the code to populate your filters, and enable the search queries here in search.js
 // TODO: You will also interact with the map.js file here
@@ -20,7 +20,7 @@ Filter.searchBox = function() {
           alert('That zip code does not exist in our database, please try another zip code.');
           zipCodes = null;
         } else {
-
+          dropMarkers(zipCodes[0].latitude, zipCodes[0].longitude);
         }
       })
     }
@@ -50,14 +50,49 @@ Filter.setCity = function() {
 }
 
 Filter.populateCity = function(ele) {
-    webDB.execute('SELECT DISTINCT city FROM zips WHERE state =' + ele + ' ORDER BY city ASC;', function(cities) {
-      var cityArr = [];
-      cities.map(function(a) {
-        cityArr.push(a.city);
-      })
-      cityArr.forEach(function(name) {
-        $('#city-select').append('<option value="' + name + '">' + name + '</option>');
-      })
+  webDB.execute('SELECT DISTINCT city FROM zips WHERE state =' + ele + ' ORDER BY city ASC;', function(cities) {
+    var cityArr = [];
+    cities.map(function(a) {
+      cityArr.push(a.city);
+    })
+    cityArr.forEach(function(name) {
+      $('#city-select').append('<option value="' + name + '">' + name + '</option>');
+    })
+  })
+  Filter.selectCity();
+}
+
+Filter.selectCity = function() {
+    var resultCity;
+    var resultState = $('#state-select').val();
+    $('#city-select').on('change', function() {
+      resultCity = $(this).val();
+      webDB.execute('SELECT * FROM zips WHERE city =' + '"' + resultCity + '"' + ' AND state =' + '"' + resultState + '";',
+        function(cities) {
+          var marker = {};
+          var myLatlng = {};
+          for (var i = 0; i < cities.length; i++) {
+            console.log(marker);
+            var myLatlng2 = {
+              'lat': cities[0][1],
+              'lng': cities[0][2]
+            }
+            console.log(cities[i][1], cities[i][2]);
+
+            var mapOptions = {
+              zoom: 4,
+              center: myLatlng2
+            }
+            var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+            marker["marker" + i] = new google.maps.Marker({
+              position: myLatlng["myLatlng" + i] = new google.maps.LatLng(cities[i][1], cities[i][2]),
+              map: map,
+            });
+            // console.log(cities[i]);
+            // dropMarkers(cities[i].latitude, cities[i].longitude);
+          }
+
+        })
     })
   }
   // })(window)
