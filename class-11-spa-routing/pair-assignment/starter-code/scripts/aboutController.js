48ef6383adf5a27a8a0c50157a362891fabf81f4
James Wortley <jameswortley@James-Wortleys-MacBook-Pro.local>
2016-07-03 10:41:31 -0700

    completed ToDo in index, articleController, routes.js

--- class-11-spa-routing/pair-assignment/starter-code/scripts/aboutController.js
+++ class-11-spa-routing/pair-assignment/starter-code/scripts/aboutController.js
@@ -1,13 +1,11 @@
-// TODO: Define a function that hides all main section elements, and then reveals just the #about section:
+// TODO: (DONE) Define a function that hides all main section elements, and then reveals just the #about section:
 (function(module) {
     var aboutController = {};
     Article.createTable();
     aboutController.index = function() {
       Article.fetchAll(articleView.initIndexPage),
         $("main > section").hide(),
-        $("#articles").show()
+        $("#about").show()
     }
-  };
-
   module.aboutController = aboutController;
 })(window);
--- class-11-spa-routing/pair-assignment/starter-code/scripts/articleController.js
+++ class-11-spa-routing/pair-assignment/starter-code/scripts/articleController.js
@@ -2,13 +2,16 @@
   var articlesController = {};
 
   // TODO: Create the `articles` table when the controller first loads, with the code that used to be in index.html:
-
+  Article.createTable();
   // TODO: Setup a function that kicks off the fetching and rendering of articles, using the same
   // code that used to be in index.html.
+  Article.fetchAll(articleView.initIndexPage);
   // TODO: Also be sure to hide all the main section elements, and reveal the #articles section:
   articlesController.index = function() {
-
+    $('main > section').hide();
+    $('#articles').show();
   };
+
 
   module.articlesController = articlesController;
 })(window);
